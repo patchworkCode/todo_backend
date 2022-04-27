2fafe9ced52cfc9d25ae29abbe691ea0c88e5044
patchworkCode <dahm326@aucklanduni.ac.nz>
2022-04-28 11:28:34 +1200

    restructuring data models to work with uuid

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use axum::{
 use serde::{Serialize, Deserialize};
 use mongodb::{Client, options::ClientOptions, Database};
 use futures::{TryStreamExt, TryFutureExt};
+use uuid::Uuid;
+use bson::serde_helpers::uuid_as_binary;
+
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
@@ -47,14 +50,14 @@ async fn hello_handler() -> Html<&'static str> {
 
 async fn dynamic(
     extract::Path(id): extract::Path<String>,
-    extract::Extension(db) : extract::Extension<Database>) -> Json<Vec<Item>> {
+    extract::Extension(db) : extract::Extension<Database>) -> Json<Vec<PayloadItem>> {
     let all_items = retrieve_all(&db).await.unwrap();
     Json(all_items)
 }
 
-async fn retrieve_all(db: &Database) -> mongodb::error::Result<Vec<Item>> {
-    let mut all_items: Vec<Item> = vec![];
-    let typed_collection = db.collection::<Item>("items");
+async fn retrieve_all(db: &Database) -> mongodb::error::Result<Vec<PayloadItem>> {
+    let mut all_items: Vec<PayloadItem> = vec![];
+    let typed_collection = db.collection::<PayloadItem>("items");
     let mut cursor = typed_collection.find(None, None).await?;
 
     while let Some(item) = cursor.try_next().await? {
@@ -65,23 +68,58 @@ async fn retrieve_all(db: &Database) -> mongodb::error::Result<Vec<Item>> {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Item {
+struct PayloadItem {
     text: String,
     complete: bool,
 }
 
+impl From <Item> for PayloadItem {
+    fn from(i: Item) -> Self {
+        Self {
+            text : i.text,
+            complete : i.complete,
+        } 
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Item {
+    #[serde(with = "uuid_as_binary")]
+    id : Uuid,
+    text : String,
+    complete : bool
+}
+
+impl From<PayloadItem> for Item {
+    fn from(p: PayloadItem) -> Self {
+        Self {
+            id : Uuid::new_v4(),
+            text  : p.text,
+            complete : p.complete,
+        }
+    }
+}
+
 async fn post_dynamic(
     extract::Extension(db) : extract::Extension<Database>,
-    extract::Path(id): extract::Path<String>, extract::Json(payload) : extract::Json<Item>
-) -> Json<Item> {
-   let return_item = Item {text: payload.text, complete: payload.complete};
-    insert_db(&db, &return_item).await.unwrap();
+    extract::Path(id): extract::Path<String>, extract::Json(payload) : extract::Json<PayloadItem>
+) -> Json<PayloadItem> {
+   let return_item = Item::from(payload);
+    insert_db(&db,&return_item).await.unwrap();
     println!("Success deserializing {:#?}", return_item);
-    Json(return_item)
+    Json(PayloadItem::from(return_item))
 }
 
 async fn insert_db(db: &Database, item: &Item) -> mongodb::error::Result<()> {
     let typed_collection = db.collection::<Item>("items");
     typed_collection.insert_one(item, None).await?;
     Ok(())
+}
+
+async fn replace(
+    extract::Extension(db) : extract::Extension<Database>,
+    extract::Path(id): extract::Path<String>,
+    extract::Json(payload) : extract::Json<PayloadItem>
+) -> Json<PayloadItem> {
+    todo!()
 }
